5ee25eb60bb802411f2be5d11d55e21d159dd511
Victor Yap <starlocke@gmail.com>
2013-06-27 12:52:43 -0400

    Added modified column to schema dump

--- Config/Schema/stickers.sql
+++ Config/Schema/stickers.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `stickers` (
   `id` char(36) DEFAULT NULL,
   `color` varchar(64) DEFAULT NULL,
-  `content` text
+  `content` text,
+  `modified` datetime
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
