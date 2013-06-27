0b856a38606dcc4ddc741e48d8d9a56a683a3666
Victor Yap <starlocke@gmail.com>
2013-06-27 08:12:52 -0400

    Added Rich Text Editor. Dropped fancy pan/scroll.

--- View/Pages/home.ctp
+++ View/Pages/home.ctp
@@ -1,4 +1,4 @@
-<article class="sticker">
+<article class="sticker" id="955af539-69f9-45e9-863d-7f85b8488907">
 	<section>
 		Hello World
 	</section>
@@ -8,7 +8,7 @@
 	</footer>
 </article>
 
-<article class="sticker">
+<article class="sticker" id="e118dbd8-58cf-4e4d-8e0f-225b6d60c351">
 	<section>
 		Hello World
 	</section>
--- webroot/js/stickers.js
+++ webroot/js/stickers.js
@@ -3,14 +3,33 @@ $(document).ready(function(){
 	window.stickers.last_position = {top: 0, left:0}; // of drag helper
 	window.stickers.last_bg_position = {top: 0, left:0};
 	window.stickers.parallax = 1.5;
-	window.stickers.init_sticker = function(elem){
-		elem.draggable({ stack: "#content .sticker" });
-		elem.find('.delete').click(function(){
-			$(this).parents('.sticker').toggle('fold', function(){
-				$(this).remove();
+	window.stickers.init_sticker = function(elems){
+		console.log(elems);
+		elems.each(function(idx, elem){
+			elem = $(elem);
+			console.log('init for ' + elem.attr('id'));
+			elem.draggable({
+				stack: "#content .sticker"
+				, containment: 'parent'
+			});
+			elem.find('.delete').click(function(){
+				$(this).parents('.sticker').toggle('fold', function(){
+					$(this).remove();
+				});
+			});
+			var html = elem.find('section').html();
+			elem.find('section').html('');
+			YUI().use('editor', function(Y) {
+				//Create the Base Editor
+				var editor = new Y.EditorBase({
+					content: html,
+					extracss: '* {background: transparent}'
+				});
+				editor.render('#' + elem.attr('id'));
 			});
 		});
 	};
+	/*
 	$( "#content" ).draggable({
 		helper: function(event){return $("<div id='content-drag-helper'></div>");}
 	, start: function(event){
@@ -30,19 +49,27 @@ $(document).ready(function(){
 			window.stickers.last_position = current_position;
 			var bg_left = (window.stickers.last_bg_position.left + diff_left/window.stickers.parallax);
 			var bg_top = (window.stickers.last_bg_position.top + diff_top/window.stickers.parallax);
-			$('#container').css({
+			$('body').css({
 				'background-position': bg_left + "px " + bg_top + 'px'
 			});
 			window.stickers.last_bg_position.left = bg_left;
 			window.stickers.last_bg_position.top = bg_top;
 		}
-	, stop: function(event){
-		}
 	});
+	*/
 	window.stickers.init_sticker($( "#content .sticker" ));
 	window.stickers.arrange = function(){
+		var width = $('#content').width();
+		var elems_per_row = Math.floor((width - 20) / 210);
+		var row = 0;
+		var column = 1;
 		$('.sticker').each(function(idx,elem){
-			$(elem).offset({ top: 40, left: idx * 210 + 10 });
+			column = idx % elems_per_row;
+			console.log('row ' + row + ' / col ' + column);
+			$(elem).offset({ top: 40 + row * 210, left: column * 210 + 10 });
+			if(column+1 == elems_per_row ){
+				row++;
+			}
 		});
 	};
 	window.stickers.arrange();
@@ -50,7 +77,15 @@ $(document).ready(function(){
 		var clone = $('.sticker.template').clone();
 		clone.removeClass('template');
 		$('#content').append(clone);
+		console.log($('#content .sticker').length);
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			return v.toString(16);
+		});
+		clone.attr('id', uuid);
 		window.stickers.init_sticker(clone);
 		window.stickers.arrange();
 	});
+	$(window).resize(window.stickers.arrange);
+
 });
